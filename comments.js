92560ae777611caa97ca9d474e7f1be2bc12f46d
Graciele Santos <osantos.graciele@gmail.com>
2024-01-25 17:16:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,103 +1,29 @@
-// Created web server
-// Created by: Mario Suarez
-// Created on: 09/10/2020
-// Last edited on: 09/10/2020
+//Create web server
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// Importing modules
+const commentsPath = path.join(__dirname, 'comments.json');
 
-const express = require('express');
-const router = express.Router();
-const Comment = require('../models/Comment');
-const User = require('../models/User');
-const { check, validationResult } = require('express-validator');
-const auth = require('../middleware/auth');
+// const comments = [
+//     {
+//         name: 'John',
+//         message: 'Hello!',
+//         timestamp: new Date()
+//     }
+// ];
 
-// @route   GET api/comments
-// @desc    Get all comments
-// @access  Public
+// const jsonString = JSON.stringify(comments, null, 2);
+// fs.writeFileSync(commentsPath, jsonString);
 
-router.get('/', async (req, res) => {
-    try {
-        const comments = await Comment.find();
-        res.json(comments);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-    }
-);
+const comments = JSON.parse(fs.readFileSync(commentsPath, 'utf-8'));
 
-// @route   POST api/comments
-// @desc    Add new comment
-// @access  Private
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
 
-router.post('/', [auth, [
-    check('comment', 'Comment is required').not().isEmpty(),
-    check('user', 'User is required').not().isEmpty(),
-    check('post', 'Post is required').not().isEmpty()
-    ]
-], async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({ errors: errors.array() });
-    }
-    const { comment, user, post } = req.body;
-    try {
-        const newComment = new Comment({
-            comment,
-            user,
-            post
-        });
-        const comment = await newComment.save();
-        res.json(comment);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-    }
-);
-
-// @route   PUT api/comments/:id
-// @desc    Update comment
-// @access  Private
-
-router.put('/:id', auth, async (req, res) => {
-    const { comment, user, post } = req.body;
-    // Build comment object
-    const commentFields = {};
-    if (comment) commentFields.comment = comment;
-    if (user) commentFields.user = user;
-    if (post) commentFields.post = post;
-    try {
-        let comment = await Comment.findById(req.params.id);
-        if (!comment) return res.status(404).json({ msg: 'Comment not found' });
-        comment = await Comment.findByIdAndUpdate(req.params.id, 
-            { $set: commentFields },
-            { new: true }
-        );
-        res.json(comment);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-    }
-);
-
-// @route   DELETE api/comments/:id
-// @desc    Delete comment
-// @access  Private
-
-router.delete('/:id', auth, async (req, res) => {
-    try {
-        let comment = await Comment.findById(req.params.id);
-        if (!comment) return res.status(404).json({ msg: 'Comment not found' });
-        await Comment.findByIdAndRemove(req.params.id);
-        res.json({ msg: 'Comment removed' });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-    }
-);
-
-module.exports = router;
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
